5911e0560cd5ce60648b601c27ac82f2d7d6b8ef
Šimun Šprem <92316028+Simun17@users.noreply.github.com>
2026-04-02 16:08:07 +0200

    feat: gcs publisher (#18)
    Co-authored-by: Luka Abramusic <91877528+labramusiq@users.noreply.github.com>
    Co-authored-by: luka.macan <luka.macan@datatonic.com>

---
+++ pkg/broker/gcs/publisher.go
@@ -0,0 +1,204 @@
+// Copyright 2024 Syntio Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcs
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/pkg/errors"
+	"google.golang.org/api/option"
+
+	"github.com/dataphos/lib-brokers/pkg/broker"
+)
+
+// Publisher models a Google Cloud Storage writer that implements the broker.Publisher interface.
+// Instead of publishing to a messaging topic, messages are written as objects to a GCS bucket.
+type Publisher struct {
+	client               *storage.Client
+	settings             PublishSettings
+	skipBucketHealthCheck bool
+}
+
+// PublisherConfig defines configuration properties needed for initializing a GCS publisher.
+type PublisherConfig struct {
+	// ProjectID is the unique identifier of the GCP project.
+	ProjectID string
+
+	// CredentialsFile optionally sets the path to a service account JSON key file.
+	// If empty, Application Default Credentials are used.
+	CredentialsFile string
+
+	// Endpoint optionally overrides the default GCS endpoint (useful for emulators/testing).
+	Endpoint string
+
+	// StorageClient optionally provides a pre-configured *storage.Client.
+	// When set, CredentialsFile and Endpoint are ignored.
+	// Useful for testing with fake GCS servers.
+	StorageClient *storage.Client
+}
+
+// PublishSettings defines optional settings for the GCS publisher.
+type PublishSettings struct {
+	// ObjectPrefix is prepended to every generated object name.
+	ObjectPrefix string
+
+	// ContentType is the MIME type set on uploaded objects.
+	ContentType string
+
+}
+
+// DefaultPublishSettings stores the default values for PublishSettings.
+var DefaultPublishSettings = PublishSettings{
+	ObjectPrefix: "",
+	ContentType:  "application/octet-stream",
+}
+
+// NewPublisher returns a new instance of Publisher, configured from the provided PublisherConfig and PublishSettings.
+func NewPublisher(ctx context.Context, config PublisherConfig, settings PublishSettings) (*Publisher, error) {
+	if config.StorageClient != nil {
+		return &Publisher{
+			client:               config.StorageClient,
+			settings:             settings,
+			skipBucketHealthCheck: true,
+		}, nil
+	}
+
+	var opts []option.ClientOption
+
+	if config.CredentialsFile != "" {
+		opts = append(opts, option.WithCredentialsFile(config.CredentialsFile))
+	}
+
+	if config.Endpoint != "" {
+		opts = append(opts, option.WithEndpoint(config.Endpoint), option.WithoutAuthentication())
+	}
+
+	client, err := storage.NewClient(ctx, opts...)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create GCS client")
+	}
+
+	return &Publisher{
+		client:   client,
+		settings: settings,
+		skipBucketHealthCheck: false,
+	}, nil
+}
+
+// Topic returns a new broker.Topic backed by the given GCS bucket.
+// The bucketName parameter is used as the destination bucket, analogous to a topic ID in messaging brokers.
+func (p *Publisher) Topic(bucketName string) (broker.Topic, error) {
+	bucket := p.client.Bucket(bucketName)
+
+	if !p.skipBucketHealthCheck {
+		if err := doBucketHealthCheck(context.Background(), bucket); err != nil {
+			return nil, err
+		}
+	}
+
+	return &Topic{
+		bucket:   bucket,
+		settings: p.settings,
+	}, nil
+}
+
+// Close closes the underlying GCS client connection.
+func (p *Publisher) Close() error {
+	return p.client.Close()
+}
+
+// Topic models a GCS bucket destination that implements the broker.Topic interface.
+type Topic struct {
+	bucket   *storage.BucketHandle
+	settings PublishSettings
+}
+
+// Publish writes a single message as a GCS object.
+// The object name is generated using the timestamp-based strategy.
+// Message attributes are stored as object metadata.
+func (t *Topic) Publish(ctx context.Context, message broker.OutboundMessage) error {
+	objectName := t.objectName(message)
+
+	writer := t.bucket.Object(objectName).NewWriter(ctx)
+	writer.ContentType = t.settings.ContentType
+
+	if message.Attributes != nil {
+		metadata := make(map[string]string, len(message.Attributes))
+
+		for key, value := range message.Attributes {
+			if v, ok := value.(string); ok {
+				metadata[key] = v
+
+				continue
+			}
+
+			metadata[key] = fmt.Sprintf("%s", value)
+		}
+
+		writer.Metadata = metadata
+	}
+
+	if _, err := writer.Write(message.Data); err != nil {
+		_ = writer.Close()
+
+		return errors.Wrap(err, "failed to write object data")
+	}
+
+	if err := writer.Close(); err != nil {
+		return errors.Wrap(err, "failed to finalize object upload")
+	}
+
+	return nil
+}
+
+// BatchPublish writes multiple messages to the GCS bucket in parallel.
+func (t *Topic) BatchPublish(ctx context.Context, messages ...broker.OutboundMessage) error {
+	return broker.SimplePublisherParallelization(ctx, t, messages...)
+}
+
+func (t *Topic) objectName(message broker.OutboundMessage) string {
+	suffix := randomHex(8)
+	ts := time.Now().UTC().Format("2006/01/02/15")
+
+	var key string
+	if message.Key != "" {
+		key = message.Key + "_"
+	}
+
+	return fmt.Sprintf("%s%s/%s%s", t.settings.ObjectPrefix, ts, key, suffix)
+}
+
+func randomHex(n int) string {
+	b := make([]byte, n)
+
+	_, _ = rand.Read(b)
+
+	return hex.EncodeToString(b)
+}
+
+// doBucketHealthCheck checks if the bucket resource exists and then checks if the service account linked to it
+// has sufficient permissions for writing objects.
+func doBucketHealthCheck(ctx context.Context, bucket *storage.BucketHandle) error {
+	_, err := bucket.Attrs(ctx)
+	if err != nil {
+		return errors.Wrap(err, "bucket is not accessible")
+	}
+	return testPermissions(ctx, bucket.IAM(), []string{"storage.objects.create"})
+}
---
+++ pkg/broker/gcs/publisher_test.go
@@ -0,0 +1,305 @@
+// Copyright 2024 Syntio Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcs_test
+
+import (
+	"context"
+	"io"
+	"strconv"
+	"testing"
+
+	"cloud.google.com/go/storage"
+	"github.com/fsouza/fake-gcs-server/fakestorage"
+	"google.golang.org/api/iterator"
+
+	"github.com/dataphos/lib-brokers/pkg/broker"
+	gcs "github.com/dataphos/lib-brokers/pkg/broker/gcs"
+)
+
+func TestNewPublisher(t *testing.T) {
+	t.Parallel()
+	server, client := setupFakeServer(t, "test-bucket","test-bucket-2")
+	defer server.Stop()
+	t.Log(listBuckets(t,client,"new-client-test1"))
+	settings := gcs.DefaultPublishSettings
+	config := gcs.PublisherConfig{
+		ProjectID:     "new-client-test",
+		StorageClient: client,
+	}
+
+	publisher, err := gcs.NewPublisher(context.Background(), config, settings)
+	if err != nil {
+		t.Fatal("Error while creating publisher: ", err)
+	}
+
+	err = publisher.Close()
+	if err != nil {
+		t.Fatal("Error while closing publisher: ", err)
+	}
+}
+
+func TestPublishOneMessage(t *testing.T) {
+	t.Parallel()
+
+	server, client := setupFakeServer(t, "publish-test-bucket")
+	defer server.Stop()
+
+	publisher, err := gcs.NewPublisher(context.Background(), gcs.PublisherConfig{
+		ProjectID:     "publish-one-message-test",
+		StorageClient: client,
+	}, gcs.DefaultPublishSettings)
+	if err != nil {
+		t.Fatal("Error while creating publisher: ", err)
+	}
+	defer publisher.Close()
+
+	topic, err := publisher.Topic("publish-test-bucket")
+	if err != nil {
+		t.Fatal("Error while referencing bucket: ", err)
+	}
+
+	message := broker.OutboundMessage{
+		Key:        "key1",
+		Data:       []byte("Simple message."),
+		Attributes: nil,
+	}
+
+	err = topic.Publish(context.Background(), message)
+	if err != nil {
+		t.Fatal("Error while publishing message: ", err)
+	}
+
+	objects := listBucketObjects(t, client, "publish-test-bucket")
+	t.Log("Objects: ", objects)
+	if len(objects) != 1 {
+		t.Fatalf("expected 1 object, got %d", len(objects))
+	}
+
+	data := readObject(t, client, "publish-test-bucket", objects[0])
+	t.Log("Data: ", string(data))
+	if string(data) != string(message.Data) {
+		t.Fatalf("expected %s, got %s", message.Data, data)
+	}
+}
+
+func TestPublishMoreMessages(t *testing.T) {
+	t.Parallel()
+
+	server, client := setupFakeServer(t, "publish-more-messages-bucket")
+	defer server.Stop()
+
+	publisher, err := gcs.NewPublisher(context.Background(), gcs.PublisherConfig{
+		ProjectID:     "publish-more-messages-test",
+		StorageClient: client,
+	}, gcs.DefaultPublishSettings)
+	if err != nil {
+		t.Fatal("Error while creating publisher: ", err)
+	}
+	defer publisher.Close()
+
+	topic, err := publisher.Topic("publish-more-messages-bucket")
+	if err != nil {
+		t.Fatal("Error while referencing bucket: ", err)
+	}
+
+	messages := []broker.OutboundMessage{
+		{
+			Key:        "key1",
+			Data:       []byte("First message."),
+			Attributes: nil,
+		},
+		{
+			Key:        "key2",
+			Data:       []byte("Second message."),
+			Attributes: nil,
+		},
+		{
+			Key:        "key3",
+			Data:       []byte("Third message."),
+			Attributes: nil,
+		},
+	}
+
+	for _, message := range messages {
+		err = topic.Publish(context.Background(), message)
+		if err != nil {
+			t.Fatal("Error while publishing message: ", err)
+		}
+	}
+
+	objects := listBucketObjects(t, client, "publish-more-messages-bucket")
+	if len(objects) != 3 {
+		t.Fatalf("expected 3 objects, got %d", len(objects))
+	}
+
+	payloads := collectObjectPayloads(t, client, "publish-more-messages-bucket", objects)
+
+	for _, message := range messages {
+		if _, found := payloads[string(message.Data)]; !found {
+			t.Fatalf("Message %s not found in bucket", string(message.Data))
+		}
+	}
+}
+
+func TestPublishBatch(t *testing.T) {
+	t.Parallel()
+
+	server, client := setupFakeServer(t, "batch-publish-bucket")
+	defer server.Stop()
+
+	publisher, err := gcs.NewPublisher(context.Background(), gcs.PublisherConfig{
+		ProjectID:     "publish-batch-test",
+		StorageClient: client,
+	}, gcs.DefaultPublishSettings)
+	if err != nil {
+		t.Fatal("Error while creating publisher: ", err)
+	}
+	defer publisher.Close()
+
+	topic, err := publisher.Topic("batch-publish-bucket")
+	if err != nil {
+		t.Fatal("Error while referencing bucket: ", err)
+	}
+
+	numMsgs := 50
+	messages := make([]broker.OutboundMessage, 0, numMsgs)
+
+	for i := 0; i < numMsgs; i++ {
+		messages = append(messages, broker.OutboundMessage{
+			Key:        "key",
+			Data:       []byte(strconv.Itoa(i)),
+			Attributes: nil,
+		})
+	}
+
+	err = topic.BatchPublish(context.Background(), messages...)
+	if err != nil {
+		t.Fatal("Error while batch publishing: ", err)
+	}
+
+	objects := listBucketObjects(t, client, "batch-publish-bucket")
+	if len(objects) != numMsgs {
+		t.Fatalf("expected %d objects, got %d", numMsgs, len(objects))
+	}
+
+	payloads := collectObjectPayloads(t, client, "batch-publish-bucket", objects)
+
+	for _, message := range messages {
+		dataString := string(message.Data)
+
+		if _, found := payloads[dataString]; !found {
+			t.Fatalf("Message %s not found in bucket", dataString)
+		}
+	}
+}
+
+func listBuckets(t *testing.T, client *storage.Client, projectID string) []string {
+	t.Helper()
+
+	var names []string
+
+	it := client.Buckets(context.Background(), projectID)
+
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil {
+			t.Fatal("Error listing buckets: ", err)
+		}
+
+		names = append(names, attrs.Name)
+	}
+
+	return names
+}
+
+func listBucketObjects(t *testing.T, client *storage.Client, bucket string) []string {
+	t.Helper()
+
+	var names []string
+
+	it := client.Bucket(bucket).Objects(context.Background(), nil)
+
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil {
+			t.Fatal("Error listing objects: ", err)
+		}
+
+		names = append(names, attrs.Name)
+	}
+
+	return names
+}
+
+func readObject(t *testing.T, client *storage.Client, bucket, object string) []byte {
+	t.Helper()
+
+	reader, err := client.Bucket(bucket).Object(object).NewReader(context.Background())
+	if err != nil {
+		t.Fatal("Error creating object reader: ", err)
+	}
+	defer reader.Close()
+
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		t.Fatal("Error reading object data: ", err)
+	}
+
+	return data
+}
+
+func readObjectAttrs(t *testing.T, client *storage.Client, bucket, object string) *storage.ObjectAttrs {
+	t.Helper()
+
+	attrs, err := client.Bucket(bucket).Object(object).Attrs(context.Background())
+	if err != nil {
+		t.Fatal("Error reading object attributes: ", err)
+	}
+
+	return attrs
+}
+
+func collectObjectPayloads(t *testing.T, client *storage.Client, bucket string, objects []string) map[string]struct{} {
+	t.Helper()
+
+	payloads := make(map[string]struct{}, len(objects))
+
+	for _, obj := range objects {
+		data := readObject(t, client, bucket, obj)
+		payloads[string(data)] = struct{}{}
+	}
+
+	return payloads
+}
+
+func setupFakeServer(t *testing.T, buckets ...string) (*fakestorage.Server, *storage.Client) {
+	t.Helper()
+
+	server := fakestorage.NewServer([]fakestorage.Object{})
+
+	for _, b := range buckets {
+		server.CreateBucketWithOpts(fakestorage.CreateBucketOpts{Name: b})
+	}
+
+	return server, server.Client()
+}
