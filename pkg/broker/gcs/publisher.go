8823f1768c44b52a9712a1f0ae4ea96dbb73283d
antonijom <89011704+antonijom@users.noreply.github.com>
2026-04-10 15:29:40 +0200

    feat: kafka ssl scram (#22)

--- pkg/broker/gcs/publisher.go
+++ pkg/broker/gcs/publisher.go
@@ -182,7 +182,10 @@ func (t *Topic) objectName(message broker.OutboundMessage) string {
 		key = message.Key + "_"
 	}
 
-	return fmt.Sprintf("%s%s/%s%s", t.settings.ObjectPrefix, ts, key, suffix)
+	if t.settings.ObjectPrefix != "" {
+		return fmt.Sprintf("%s/%s/%s%s", t.settings.ObjectPrefix, ts, key, suffix)
+	}
+	return fmt.Sprintf("%s/%s%s", ts, key, suffix)
 }
 
 func randomHex(n int) string {
